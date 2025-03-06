3d055ac0c5fb7d846e4e746ce40ab9cb29552ed3
dm <>
2025-03-07 00:29:30 +0300

    drawing points and lines

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include "vector.h" 
 
+#define POINT_COLOR 1.0f, 0.0f, 0.0f
+#define LINE_COLOR 0.0f, 0.0f, 1.0f
+
 typedef struct {
    PointList list;
    int mouseDown;
@@ -11,6 +14,7 @@ typedef struct {
 void display(void);
 void mouseFunc(int button, int state, int x, int y);
 void motionFunc(int x, int y);
+void reshape(int w, int h);
 
 int main(int argc, char **argv) {
    glutInit(&argc, argv);
@@ -29,29 +33,26 @@ int main(int argc, char **argv) {
    glutDisplayFunc(display);
    glutMouseFunc(mouseFunc);
    glutMotionFunc(motionFunc);
+   glutReshapeFunc(reshape);
 
    glutMainLoop();
    return 0;
 }
 
 
-
-void display(void) {
-   glClear(GL_COLOR_BUFFER_BIT);
-   glutSwapBuffers();
-}
-
 void mouseFunc(int button, int state, int x, int y) {
    AppState *stateVars = (AppState *)glutGetWindowData();
    if (state == GLUT_DOWN) {
       printf("Mouse button %d pressed at (%d, %d)\n", button, x, y);
       stateVars->mouseDown = 1;
+      if(stateVars->list.points != NULL){
+	 free(stateVars->list.points);
+	 stateVars->list = (PointList){NULL, 0};
+      }
    } else if (state == GLUT_UP) {
       printf("Mouse button %d released at (%d, %d)\n", button, x, y);
       stateVars->mouseDown = 0;
       printPointList(stateVars->list);
-      free(stateVars->list.points);
-      stateVars->list = (PointList){NULL, 0};
    }
 }
 
@@ -61,4 +62,42 @@ void motionFunc(int x, int y) {
    if (state->mouseDown) {
       appendPointList(&state->list, (Point){(float)x, (float)y});
    }
+
+   glutPostRedisplay();
+}
+
+void display(void) {
+   glClear(GL_COLOR_BUFFER_BIT);
+   
+   AppState *state = (AppState *)glutGetWindowData();
+   
+
+   if (state->list.points != NULL && state->list.size > 0) {
+      glColor3f(LINE_COLOR);
+      glPointSize(2.f);
+      glBegin(GL_LINE_STRIP);
+      for (size_t i = 0; i < state->list.size; i++) {
+	 glVertex2f(state->list.points[i].x, state->list.points[i].y);
+      }
+      glEnd();
+
+      glColor3f(POINT_COLOR);
+      glPointSize(5.f);
+      glBegin(GL_POINTS);
+      for (size_t i = 0; i < state->list.size; i++) {
+	 glVertex2f(state->list.points[i].x, state->list.points[i].y);
+      }
+      glEnd();
+   }
+
+   glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, w, h, 0, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
